6c891fa7155467c8ffce77c822a55b00910fdaa0
Your Name <you@example.com>
2024-01-03 20:01:01 +0300

    add home list

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     author=models.CharField(max_length=255,default='admin')
     description=models.TextField(blank=True)
     image=models.ImageField(upload_to='image/')
-    price=models.DecimalField(max_digits=4,decimal_places=2)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
     in_stock=models.BooleanField(default=True)
     is_active=models.BooleanField(default=True)
     created=models.DateTimeField(auto_now_add=True)
